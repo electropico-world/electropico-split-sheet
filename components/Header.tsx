b860f65f1030b73c3ed26e2364cf5e0f641b8295
electropico-world <electropicomusic@gmail.com>
2026-08-05 01:57:43 -0400

    Use Electropico logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="topbar">
-      <Link href="/" className="brand">
-        <span className="brand-mark">EP</span>
-        <span>ELECTROPICO SPLITS</span>
+      <Link href="/" className="brand brand-logo-link" aria-label="Electropico Splits home">
+        <img className="brand-logo" src="/electropico-logo.svg" alt="Electropico Records" />
+        <span className="brand-title">SPLITS</span>
       </Link>
       <div className="nav-actions">
         <Link className="btn btn-secondary" href="/agreements/new">New agreement</Link>
